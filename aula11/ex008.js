8e566a16764a4db738eb3bad8ca9c07c04bdee08
carlinhosrb <91791858+carlinhosrb@users.noreply.github.com>
2023-02-17 14:12:45 -0300

    if else

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,2 +1,6 @@
-var vel = 60.5
+var vel = 80
 console.log(`a velocidade é ${vel} km/h`)
+if (vel > 60){//condição simples
+    console.log(`MULTADO!!`)
+}
+console.log(`Utilize o cinto de segurança!`)
