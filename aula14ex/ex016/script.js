a9f4ac068d273153bb8683443d8f2c30e811edd3
carlinhosrb <91791858+carlinhosrb@users.noreply.github.com>
2023-03-27 12:02:08 -0300

    ex 16

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,10 @@
-alert('Olá')
+function contar(){
+    var inicio = document.getElementById('txtinicio')
+    var fim = document.getElementById('txtfim')
+    var passo = document.getElementById('txtpasso')
+    var res = document.getElementById('resultado')
+
+    if(inicio == 0){
+        res.innerHTML = `Impossivel contar !`
+    }
+}
