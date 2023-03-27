14caac7a27e52f9a803f0d446e18213c40c5bd84
Carlos Alexandre <91791858+carlinhosrb@users.noreply.github.com>
2023-03-26 21:44:40 -0300

    indexOf()

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -9,3 +9,12 @@ console.log(`Nosso vetor tem ${num.length} posições`)
 for (let pos=0; pos <num.length; pos++) {
     console.log(`O valor do elemento ${pos} é ${num[pos]}`)
 }
+
+console.log()
+let pos = num.indexOf(8)
+if (pos == -1){
+    console.log('O valor não foi encontrado')
+}
+else{
+    console.log(`O valor 8 esta na posição ${pos}`)
+}
