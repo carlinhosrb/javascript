c146b175890152e51b10576ebcc1d95dfc74c519
carlinhosrb <91791858+carlinhosrb@users.noreply.github.com>
2023-05-04 09:00:57 -0300

    Update criptografia.js

--- cifra/criptografia.js
+++ cifra/criptografia.js
@@ -28,30 +28,31 @@ String.fromCharCode(72)
 */
 
 
+var frase ='carlos';                                //TEXTO ORIGINAL
+frase = frase.toUpperCase();                        //CONVERTE TODO TEXTO EM CAPS LOCK
 
-let alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-var frase ='carlos';
-var tamanhoFrase = frase.length;
-var codigoDaletra = frase;
+let alfabeto = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var tamanhoFrase = frase.length;                    //TAMANHO DO TEXTO
+var codigoDaletra = frase;                          
+let texto = []
 let cifra = []
-let cripto = []
 
 for(i=0; i< tamanhoFrase; i++){
 
     let letra = frase[i];
     let deslocamento = 7;
 
-    let teste = ((letra.charCodeAt(0))-97);
+    let codASC = ((letra.charCodeAt(0))-65);        //CONVERTE A LETRA PARA CODIGO ASC E SALVA NA VARIAVEL codASC
     //console.log(letra.charCodeAt(0));
     //console.log(String.fromCharCode(teste));
-    teste = teste + deslocamento;
-    console.log(teste)
+    codASC = codASC + deslocamento;                 //SOMA O DESLOCAMENTO + CODIGO ASC DA LETRA
+    //console.log(teste)
 
-    cifra.push(letra)
-    cripto.push(alfabeto[teste])
+    texto.push(letra)
+    cifra.push(alfabeto[codASC])
 };
+console.log(texto);
 console.log(cifra);
-console.log(cripto);
     //(codigoDaletra + desloc) % tamDoAlfabeto
 
     //((codigoDaletraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1Letra
